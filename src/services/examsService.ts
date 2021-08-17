915b820808f007826d1582adb51cfd3bee6daa01
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-08-16 22:30:57 -0300

    fixing deploy

--- src/services/examsService.ts
+++ src/services/examsService.ts
@@ -1,13 +1,10 @@
 import { getRepository } from "typeorm";
-import Exams from "../entities/Exams";
-import { Exam } from "../interfaces/Exam";
 
-import { 
-  checkNotExistingCategory, 
-  checkNotExistingDiscipline,
-  checkNotExistingProfessor,
-  checkSameExam
-} from "../../src/schemas/validateExamParams";
+import { Exam } from "../interfaces/Exam";
+import Categories from "../entities/Categories";
+import Disciplines from "../entities/Disciplines";
+import Exams from "../entities/Exams";
+import Professors from "../entities/Professors";
 
 export async function saveExam (params: Exam) {
   const checkExistingCategory = await checkNotExistingCategory(params.categoryId);
@@ -40,4 +37,42 @@ export async function getExamsByProfessor (professorId: number) {
     relations: ["discipline", "category"]
   });
   return exams;
+}
+
+async function checkNotExistingCategory(params: number): Promise<boolean> {
+    const categories = await getRepository(Categories).find();
+    const categoriesIds = categories.map(c => c.id);
+    const existingCategory = categoriesIds.find(c => params === c);
+
+    if(!existingCategory) return false
+    else return true;
+}
+
+async function checkNotExistingDiscipline(params: number): Promise<boolean> {
+  const disciplines = await getRepository(Disciplines).find();
+  const disciplinesIds = disciplines.map(d => d.id);
+  const existingDiscipline = disciplinesIds.find(d => params === d);
+
+  if(!existingDiscipline) return false
+  else return true;
+}
+
+async function checkNotExistingProfessor(params: number): Promise<boolean> {
+  const professors = await getRepository(Professors).find();
+  const professorsIds = professors.map(p => p.id);
+  const existingProfessor = professorsIds.find(p => params === p);
+
+  if(!existingProfessor) return false
+  else return true;
+}
+
+async function checkSameExam(params: string): Promise<boolean>  {
+  const exams = await getRepository(Exams).find();
+  if(exams.length !== 0){
+    const examsLinks = exams.map(e => e.examLink);
+    const existingLink = examsLinks.find(p => params === p);
+  
+    if(existingLink) return true;
+    else return false
+  }
 }
